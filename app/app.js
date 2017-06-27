255607bb40b9327a454177ac5b89c8ee50857c87
@bvmCoder <talkwithdixit@gmail.com>
2017-06-27 19:13:13 -0400

    Added WebPack Config File

--- app/app.js
+++ app/app.js
@@ -2,3 +2,21 @@ import "normalize.css/normalize.css"
 import "assets/stylesheets/main.css"
 // import { createStore } from "redux"
 const { createStore } = require("redux")
+
+const initialState = 5
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'INC':
+			return state + 1
+	}
+	return state
+}
+
+
+const store = createStore(reducer, initialState)
+
+store.subscribe(() => document.querySelector("#counter").innerText = store.getState())
+
+setInterval(() => store.dispatch({ type: 'INC'}), 500)
+
+console.log("Redux has started the Game")
